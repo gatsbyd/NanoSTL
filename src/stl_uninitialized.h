b71803e8610fea41233cde0ff28a13d2a42725d1
daihaonan <1240184197@qq.com>
2019-05-27 11:44:28 +0800

    cause: stl_uninitialized.h add test for uninitialized_xxx functions solution:done

--- src/stl_uninitialized.h
+++ src/stl_uninitialized.h
@@ -2,62 +2,105 @@
 
 #include <string>
 
-#include "src/type_traits.h"
-#include "src/iterator.h"
+// #include "src/type_traits.h"
+// #include "src/stl_iterator.h"
+#include "src/stl_construct.h"
 
 namespace NanoSTL {
 
 //uninitialized_copy()
-template <tepename InputIterator, typename ForwordIterator>
+template <typename InputIterator, typename ForwordIterator>
 inline ForwordIterator uninitialized_copy(InputIterator first, InputIterator last,
                                     ForwordIterator result) {
-    typedef typename type_traits<iterator_traits<InputIterator>::value_type>::is_POD_type is_POD_type;
-    return _uninitialized_copy_aux(first, last, result, is_POD_type());
-}
-
-template <tepename InputIterator, typename ForwordIterator>
-inline ForwordIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                    ForwordIterator result, __true_type /*unused*/) {
-    memmove(result, first, last - first);   //why not memset()
-    return  result + (last - first);        //only built-in type will go in here so we can do this
-}
-
-template <tepename InputIterator, typename ForwordIterator>
-inline ForwordIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                    ForwordIterator result, __false_type /*unused*/) {
-    while (first != end) {
-        construct(result++, *(first++));
+    //typedef typename type_traits<iterator_traits<InputIterator>::value_type>::is_POD_type is_POD_type;
+    //todo
+    while (first != last) {
+        construct(&*result, *first);
+        ++result;
+        ++first;
     }
     return result;
 }
 
+// template <typename InputIterator, typename ForwordIterator>
+// inline ForwordIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
+//                                     ForwordIterator result, __true_type /*unused*/) {
+//     memmove(result, first, last - first);   //why not memset()
+//     return  result + (last - first);        //only scalar type will go in here (see type_traits.h) so we can do this
+// }
+
+// template <typename InputIterator, typename ForwordIterator>
+// inline ForwordIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
+//                                     ForwordIterator result, __false_type /*unused*/) {
+//     while (first != end) {
+//         construct(result++, *(first++));
+//     }
+//     return result;
+// }
+
+// inline char* uninitialized_copy(const char* first, const char* last,
+//                                 char* result) {
+//   memmove(result, first, last - first);
+//   return result + (last - first);
+// }
+
+// inline wchar_t* uninitialized_copy(const wchar_t* first, const wchar_t* last,
+//                                    wchar_t* result) {
+//   memmove(result, first, sizeof(wchar_t) * (last - first));
+//   return result + (last - first);
+// }
+
 
 //uninitialized_fill()
-template <template ForwardIterator, template T>
+template <typename ForwardIterator, typename T>
 inline void uninitialized_fill(ForwardIterator first, ForwardIterator last, 
                                const T& x) {
-    typedef typename type_traits<iterator_traits<InputIterator>::value_type>::is_POD_type is_POD_type;
-    return _uninitialized_fill_aux(first, last, x, is_POD_type());
-}
-
-template <template ForwardIterator, template T>
-inline void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, 
-                                                const T& x, __true_type /*unused*/) {
-    typedef typename iterator_traits<ForwardIterator>::value_type value_type;
-    memset(first, x, sizeof(value_type) * (last- first));
-}
-
-template <template ForwardIterator, template T>
-inline void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, 
-                                                const T& x, __false_type /*unused*/) {
+    //typedef typename type_traits<iterator_traits<InputIterator>::value_type>::is_POD_type is_POD_type;
+    //todo
     for (auto cur = first; cur != last; ++cur) {
-        construct(cur, x);
+        construct(&*cur, x);
     }
 }
 
+// template <typename ForwardIterator, typename T>
+// inline void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, 
+//                                                 const T& x, __true_type /*unused*/) {
+//     typedef typename iterator_traits<ForwardIterator>::value_type value_type;
+//     memset(first, x, sizeof(value_type) * (last- first));
+// }
+
+// template <typename ForwardIterator, typename T>
+// inline void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, 
+//                                                 const T& x, __false_type /*unused*/) {
+//     for (auto cur = first; cur != last; ++cur) {
+//         construct(cur, x);
+//     }
+// }
+
 //uninitialized_fill_n()
-template <template ForwardIterator, template Size, template T>
+template <typename ForwardIterator, typename Size, typename T>
 inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n,
                                             const T& x) {
-    //todo
+    //typedef typename type_traits<iterator_traits<ForwardIterator>::value_type>::is_POD_type is_POD_type;
+    //todo 
+  ForwardIterator cur = first;
+    for ( ; n > 0; --n, ++cur)
+        construct(&*cur, x);
+    return cur;
+}
+
+// template <typename ForwardIterator, typename Size, typename T>
+// inline ForwardIterator uninitialized_fill_n_axu(ForwardIterator first, Size n,
+//                                             const T& x, __true_type /*unused*/) {
+//     typedef typename iterator_traits<ForwardIterator>::value_type value_type;
+//     memset(first, x, szieof(value_type) * n);
+//     return first + n;                                        
+// }
+
+// template <typename ForwardIterator, typename Size, typename T>
+// inline ForwardIterator uninitialized_fill_n_axu(ForwardIterator first, Size n,
+//                                             const T& x, __true_type /*unused*/) {
+    
+// }
+
 }
---
+++ test/uninitialized_test.cpp
@@ -0,0 +1,50 @@
+#include "src/stl_alloc.h"
+#include "src/stl_uninitialized.h"
+#include "gtest/gtest.h"
+
+#include <vector>
+
+namespace NanoSTL {
+
+struct Foo {
+	Foo(int a) :a_(a) {}
+	int a_;
+};
+
+
+TEST(UninitializedTest, BaseTest) {
+	const int size = 5;
+
+	int ia1[size] = {1, 2, 3, 4, 5};
+	int *ip1 = simple_alloc<int, alloc>::allocate(size);
+	uninitialized_copy(ia1, ia1+size, ip1);
+	for (int i = 0; i < size; i++) {
+		EXPECT_EQ(ia1[i], ip1[i]);
+	}
+
+	int *ip2 = simple_alloc<int, alloc>::allocate(size);
+	uninitialized_fill(ip2, ip2+size, 100);
+	for (int i = 0; i < size; i++) {
+		EXPECT_EQ(100, ip2[i]);
+	}
+
+	int *ip3 = simple_alloc<int, alloc>::allocate(size);
+	uninitialized_fill_n(ip3, size, 100);
+	for (int i = 0; i < size; i++) {
+		EXPECT_EQ(100, ip3[i]);
+	}
+
+	std::vector<Foo> vf = {Foo(1), Foo(2), Foo(3), Foo(4), Foo(5)};
+	Foo *fp = simple_alloc<Foo, alloc>::allocate(size);
+	NanoSTL::uninitialized_copy(vf.begin(), vf.end(), fp); //ADL.https://stackoverflow.com/questions/8111677/what-is-argument-dependent-lookup-aka-adl-or-koenig-lookup
+	for (int i = 0; i < size; i++) {
+		EXPECT_EQ(vf[i].a_, fp[i].a_);
+	}
+
+
+	//destroy
+}
+
+}
+
+
